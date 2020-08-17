9667f4b55511cfcd93e8a01579932902b8382036
vibhor166 <52551223+vibhor166@users.noreply.github.com>
2020-08-16 21:30:21 -0400

    test

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
 //Serve static assets in production
-if (process.env.NODE_env === 'production') {
+if (process.env.NODE_ENV === 'production') {
   //Set static folder
   app.use(express.static('client/build'));
   app.get('*', (req, res) => {
